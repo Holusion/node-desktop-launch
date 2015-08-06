9f5f9c5ff7f40af7aefe6efca8357b77706de8aa
Sebastien Dumetz <sebastien.dumetz@isen-lille.fr>
2015-08-06 18:15:45 +0200

    impl exec function for app launchers

--- test/test_launcher.js
+++ test/test_launcher.js
@@ -1,37 +1,49 @@
 var Launcher = require("../lib");
+describe("Launcher",function(){
+  describe(".start()",function(){
+    beforeEach(function(){
+      this.launcher = new Launcher();
 
-describe("Launcher.start",function(){
-  it("simple",function(done){
-    var launcher = new Launcher();
-    launcher.exec = function(command,entry){
-      expect(command).to.equal("/path/to/file.txt");
-      expect(entry).to.equal("fooview %F");
-      done();
-    }
-    launcher.start("/path/to/file.txt").catch(function(e){
-      done(e);
+    });
+    it("simple",function(done){
+      this.launcher.exec = function(command,entry){
+        expect(command).to.equal("/path/to/file.txt");
+        expect(entry).to.equal("fooview %F");
+        done();
+      }
+      this.launcher.start("/path/to/file.txt").catch(function(e){
+        done(e);
+      });
+    });
+    it("binary",function(done){
+      this.launcher.exec = function(command,entry){
+        expect(command).to.equal("/path/to/file");
+        expect(entry).to.be.null;
+        done();
+      }
+      this.launcher.start("/path/to/file").catch(function(e){
+        done(e);
+      });
+    });
+    it("known format without autolaunch",function(done){
+      this.launcher.exec = function(command,entry){
+        expect(command).to.equal("/path/to/file.foo");
+        expect(entry).to.equal("fooview %F");
+        done();
+      }
+      this.launcher.start("/path/to/file.foo").catch(function(e){
+        done(e);
+      });
     });
   });
-  it("binary",function(done){
-    var launcher = new Launcher();
-    launcher.exec = function(command,entry){
-      expect(command).to.equal("/path/to/file");
-      expect(entry).to.be.null;
-      done();
-    }
-    launcher.start("/path/to/file").catch(function(e){
-      done(e);
+  describe(".exec",function(){
+    beforeEach(function(){
+      this.launcher = new Launcher();
     });
-  });
-  it("known format without autolaunch",function(done){
-    var launcher = new Launcher();
-    launcher.exec = function(command,entry){
-      expect(command).to.equal("/path/to/file.foo");
-      expect(entry).to.equal("fooview %F");
-      done();
-    }
-    launcher.start("/path/to/file.foo").catch(function(e){
-      done(e);
+    it("parse commands",function(){
+      this.launcher.exec("1","sleep %F");
+      expect(this.launcher.child).to.be.not.null;
+      expect(process.kill(this.launcher.child.pid),0).to.be.true;
     });
   });
 })
