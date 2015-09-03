2e05a54c392979c43f3b86e3bc731702a491eb11
Sebastien Dumetz <s.dumetz@holusion.com>
2015-09-03 16:05:29 +0200

    fixed urls and array of files parameters

--- test/test_make_command_line.js
+++ test/test_make_command_line.js
@@ -1,11 +1,21 @@
 describe("makeCommandLine",function(){
   var make = require("../lib/utils/makeCommandLine");
   var lines = ["foo %F","foo %U","foo %k %n %m %u"];
-  var file = "foofile";
-  var result = {exec:"foo",params: ["foofile"]}
-  lines.forEach(function(line,index){
-    it("parse :"+line,function(){
-      expect(make(file,line)).to.deep.equal(result);
+
+  var testFn = function(file, lines, results){
+    lines.forEach(function(line,index){
+      it(line,function(){
+        expect(make(file,line)).to.deep.equal(results[index]);
+      });
     });
-  });
+  }
+  describe("absolute",function(){
+    var results = [{exec:"foo",params: ["/foofile"]},{exec:"foo",params: ["file:///foofile"]},{exec:"foo",params: ["file:///foofile"]}]
+    testFn("/foofile",lines,results);
+  })
+  describe("relative",function(){
+    var results = [{exec:"foo",params: ["foofile"]},{exec:"foo",params: ["file://foofile"]},{exec:"foo",params: ["file://foofile"]}]
+    testFn("foofile",lines,results);
+  })
+
 });
