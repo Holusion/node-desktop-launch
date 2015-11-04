4e5a1aeb3daee364788c544c34b33c7a1c03642c
Sebastien Dumetz <s.dumetz@holusion.com>
2015-11-04 11:25:27 +0100

    add a test for path argumetns with spaces

--- test/test_make_command_line.js
+++ test/test_make_command_line.js
@@ -1,21 +1,28 @@
 describe("makeCommandLine",function(){
   var make = require("../lib/utils/makeCommandLine");
-  var lines = ["foo %F","foo %U","foo %k %n %m %u"];
+  describe("parse paths",function(){
 
-  var testFn = function(file, lines, results){
-    lines.forEach(function(line,index){
-      it(line,function(){
-        expect(make(file,line)).to.deep.equal(results[index]);
+    var lines = ["foo %F","foo %U","foo %k %n %m %u"];
+
+    var testFn = function(file, lines, results){
+      lines.forEach(function(line,index){
+        it(line,function(){
+          expect(make(file,line)).to.deep.equal(results[index]);
+        });
       });
+    }
+    describe("absolute",function(){
+      var results = [{exec:"foo",params: ["/foofile"]},{exec:"foo",params: ["file:///foofile"]},{exec:"foo",params: ["file:///foofile"]}]
+      testFn("/foofile",lines,results);
+    })
+    describe("relative",function(){
+      var results = [{exec:"foo",params: ["foofile"]},{exec:"foo",params: ["file://foofile"]},{exec:"foo",params: ["file://foofile"]}]
+      testFn("foofile",lines,results);
     });
-  }
-  describe("absolute",function(){
-    var results = [{exec:"foo",params: ["/foofile"]},{exec:"foo",params: ["file:///foofile"]},{exec:"foo",params: ["file:///foofile"]}]
-    testFn("/foofile",lines,results);
+  });
+  describe("escape args",function(){
+    it("spaces",function(){
+      expect(make("test with spaces","foo %F")).to.deep.equal({exec:"foo",params: ["test with spaces"]});
+    });
   })
-  describe("relative",function(){
-    var results = [{exec:"foo",params: ["foofile"]},{exec:"foo",params: ["file://foofile"]},{exec:"foo",params: ["file://foofile"]}]
-    testFn("foofile",lines,results);
-  })
-
 });
