373964bf6567598de375861d11a76fde05f1b4af
Sebastien Dumetz <s.dumetz@holusion.com>
2019-06-25 15:34:01 +0200

    fix a bug where some characters were not escaped in file: URIs

--- test/test_make_command_line.js
+++ test/test_make_command_line.js
@@ -7,21 +7,23 @@ describe("makeCommandLine",function(){
     it("absolute path to file",function(){
       expect(make("/path/to/file", "foo %f")).to.deep.equal({exec:"foo", params:["/path/to/file"]});
     })
+    it("path to file with reserved chars",function(){
+      expect(make("/path/to/file", "foo %f")).to.deep.equal({exec:"foo", params:["/path/to/file"]});
+    })
     it("array of absolute file paths",function(){
       expect(make(["/path/to/file", "/path/to/file2"], "foo %F")).to.deep.equal({exec:"foo", params:["/path/to/file", "/path/to/file2"]});
     })
     
     it("relative path to URL",function(){
-      expect(make("path/to/file", "foo %u")).to.deep.equal({exec:"foo", params:["file:///path/to/file"]});
+      expect(make("path/to/file", "foo %u")).to.deep.equal({exec:"foo", params:["path/to/file"]});
     })
     it("absolute path to URL",function(){
-      expect(make("/path/to/file", "foo %u")).to.deep.equal({exec:"foo", params:["file:///path/to/file"]});
+      expect(make("/path/to/file", "foo %u")).to.deep.equal({exec:"foo", params:["/path/to/file"]});
     })
     it("array of paths to URLs",function(){
-      expect(make("/path/to/file", "foo %U")).to.deep.equal({exec:"foo", params:["file:///path/to/file"]});
-      expect(make(["/path/to/file", "/path/to/file2"], "foo %U")).to.deep.equal({exec:"foo", params:["file:///path/to/file", "file:///path/to/file2"]});
+      expect(make("/path/to/file", "foo %U")).to.deep.equal({exec:"foo", params:["/path/to/file"]});
+      expect(make(["/path/to/file", "/path/to/file2"], "foo %U")).to.deep.equal({exec:"foo", params:["/path/to/file", "/path/to/file2"]});
     })
-
     it("URL with custom protocol", function(){
       expect(make("foo:///path/to/file", "foo %u")).to.deep.equal({exec:"foo", params:["foo:///path/to/file"]})
     })
@@ -35,13 +37,13 @@ describe("makeCommandLine",function(){
     });
 
     it("accepts escaped entries",function(){
-      expect(make("file.txt", `foo "something escaped" %u`)).to.deep.equal({exec:"foo",params: ["something escaped","file:///file.txt"]})
+      expect(make("file.txt", `foo "something escaped" %u`)).to.deep.equal({exec:"foo",params: ["something escaped","file.txt"]})
     })
     it("keep pre-defined args",function(){
       expect(make("file.txt","foo --bar %F")).to.deep.equal({exec:"foo",params: ["--bar", "file.txt"]});
     });
     it("remove deprecated options",function(){
-      expect(make("file.txt", "foo %n %m %u")).to.deep.equal({exec:"foo",params: ["file:///file.txt"]})
+      expect(make("file.txt", "foo %n %m %u")).to.deep.equal({exec:"foo",params: ["file.txt"]})
     })
     it("remove trailing %f, %u... options",function(){
       expect(make("file.txt", "foo %f %U %u")).to.deep.equal({exec:"foo",params: ["file.txt"]})
